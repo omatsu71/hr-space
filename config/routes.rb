025606421620d86098f07c7ca329e901570bca3d
omatsu71 <jampamam1223@gmail.com>
2020-06-07 08:48:56 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get 'tweets' => 'tweets#index'
-  get 'tweets/new' => 'tweets#new'
-  post 'tweets' => 'tweets#create'
+  # get 'tweets' => 'tweets#index'
+  # get 'tweets/new' => 'tweets#new'
+  # post 'tweets' => 'tweets#create'
+  resources :tweets do
+    collection do
+      get "create"
+      get "new"
+    end
+  end
+
+  root "tweetss#index"
 end
