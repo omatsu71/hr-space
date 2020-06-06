2fec2d1d324662112f77c29693ce7ad9491480fd
omatsu71 <jampamam1223@gmail.com>
2020-06-07 08:52:34 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  # get 'tweets' => 'tweets#index'
-  # get 'tweets/new' => 'tweets#new'
-  # post 'tweets' => 'tweets#create'
   resources :tweets do
-    collection do
-      get "create"
-      get "new"
-    end
   end
 
   root "tweetss#index"
